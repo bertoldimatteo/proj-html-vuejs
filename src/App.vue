b82202b96d1108e9bdb0838030aa91018b10545b
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-15 11:58:26 +0200

    aggiunto link componenti principali ad app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-
+    <BaseHeader/>
+    <BaseMain/>
+    <BaseFooter/>
   </div>
 </template>
 
 <script>
+import BaseFooter from './components/mainComponents/baseFooter.vue'
+import BaseHeader from './components/mainComponents/baseHeader.vue'
+import BaseMain from './components/mainComponents/baseMain.vue'
 
 
 export default {
   name: 'App',
   components: {
+    BaseHeader,
+    BaseMain,
+    BaseFooter,
 
   }
 }
