2fdfe083de2038c82872c7892c9ff4356a2000fd
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-15 15:38:43 +0200

    inizio implementazione navabr

--- src/components/mainComponents/baseMain.vue
+++ src/components/mainComponents/baseMain.vue
@@ -1,6 +1,11 @@
 <template>
     <main>
-        <div class="background-container"></div>
+        <nav>
+            <div class="left-navbar">
+                <img src="../../assets/reference/img/theme_eduprime_logo.png" alt="">
+            </div>
+            <div class="right-navbar"></div>
+        </nav>
         <div class="background-wave"></div>
     </main>
 </template>
@@ -13,18 +18,14 @@ export default {
 
 <style lang="scss" scoped>
 main {
-    background:(var(--color-three));
-    height: 880px;
-}
-.background-container {
-    background-image: url('../../assets/reference/img/theme_slider2_bg-1.jpg');
+    background-image: linear-gradient(rgba(220,109,111,0.9), rgba(220,109,111,0.9)), url('../../assets/reference/img/theme_slider2_bg-1.jpg');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: top;
     height: 880px;
-    opacity: 0.1;
     position: relative;
 }
+
 .background-wave {
     position: absolute;
     background-image: url('../../assets/reference/img/Wave-1.png');
