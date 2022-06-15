4facf0a8796e31f80330c980af0f0df56053ad2d
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-15 14:50:33 +0200

    inizio stilizzazione header

--- src/components/mainComponents/baseMain.vue
+++ src/components/mainComponents/baseMain.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
+        <div class="background-container">
 
+        </div>
     </main>
 </template>
 
@@ -11,5 +13,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+main {
+    background:(var(--color-three));
+    height: 800px;
+}
+.background-container {
+    background-image: url('../../assets/reference/img/theme_slider2_bg-1.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top;
+    height: 800px;
+    opacity: 0.1;
+}
 </style>
