cb0532bf3a60464ebbe8e9605ea68fa1872d7873
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-15 15:19:24 +0200

    aggiunto background hero

--- src/components/mainComponents/baseMain.vue
+++ src/components/mainComponents/baseMain.vue
@@ -1,8 +1,7 @@
 <template>
     <main>
-        <div class="background-container">
-
-        </div>
+        <div class="background-container"></div>
+        <div class="background-wave"></div>
     </main>
 </template>
 
@@ -15,14 +14,24 @@ export default {
 <style lang="scss" scoped>
 main {
     background:(var(--color-three));
-    height: 800px;
+    height: 850px;
 }
 .background-container {
     background-image: url('../../assets/reference/img/theme_slider2_bg-1.jpg');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: top;
-    height: 800px;
+    height: 850px;
     opacity: 0.1;
+    position: relative;
+}
+.background-wave {
+    position: absolute;
+    background-image: url('../../assets/reference/img/Wave-1.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 130px;
+    top: 720px;
 }
 </style>
